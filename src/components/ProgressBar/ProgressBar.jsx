04f4679cf694e1a3d56a2d0f4eebd18457de316f
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2023-04-21 22:24:04 +0530

    progress bar colour changed

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -7,17 +7,19 @@ const ResponsiveProgressBar = ({ percentage }) => {
   return (
     <div className="progress-bar-container">
       <ProgressBar
-        completed={percentageFunded}
-        label={`${percentageFunded}% funded`}
-        bgColor='var(--secondary)' 
-        baseBgColor='var(--white)' 
-        height='20px' 
-        width='100% !important'
-        margin='5px' 
-        isLabelVisible 
-        transition='2s ease-in'
-        className='progress-bar'
-      />
+          completed={percentageFunded}
+          label={`${percentageFunded}% funded`}
+          bgColor='var(--secondary)' 
+          baseBgColor='var(--white)' 
+          height='20px' 
+          width='100% !important'
+          margin='5px' 
+          isLabelVisible 
+          transition='2s ease-in'
+          labelStyle={{ color: 'var(--black) !important' }}
+          labelClassName='progressBar-label'
+          className='progress-bar'
+        />
       <div className="eth-text">5 ETH</div>
       <div className="eth-collected">3 ETH collected</div>
       <div className="eth-needed">2 ETH Needed</div>
