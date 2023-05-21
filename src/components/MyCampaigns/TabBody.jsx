e90f856026d57dd6c624aa3476f6d4495698fd90
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2023-05-21 13:33:31 +0530

    with errors

--- src/components/MyCampaigns/TabBody.jsx
+++ src/components/MyCampaigns/TabBody.jsx
@@ -1,37 +1,38 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-
-import Img2 from '../../assets/icons/home-card-1.jfif'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from './redux/userSlice'; 
 
 const TabBody = ({ campaigns }) => {
-    const [load, setLoad] = useState(false);
+  const [load, setLoad] = useState(false);
+  const currentUser = useSelector(selectCurrentUser);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setLoad(true);
-        }, 700)
-    })
+  useEffect(() => {
+    setTimeout(() => {
+      setLoad(true);
+    }, 700);
+  }, []);
 
-    useEffect(() => {
-        window.scrollTo(0, 700)
-    }, [])
+  useEffect(() => {
+    window.scrollTo(0, 700);
+  }, []);
 
-    return (
-        <div className="tab-body">
-            {
-                campaigns.length > 0 && campaigns.slice().reverse().map(campaign => {
-                    return (
-                        <Link to={`/campaign/${campaign.id}`} key={campaign.id}>
-                            <div className={load ? "tab-img-container" : "tab-img-container skeleton"}>
-                                <img style={{ visibility: load ? 'visible' : 'hidden' }} src={Img2} alt="" />
-                                {load && <p className='tab-img-desc text-overflow-hide'>{campaign.title}</p>}
-                            </div>
-                        </Link>
-                    )
-                })
-            }
-        </div>
-    )
-}
+  return (
+    <div className="tab-body">
+      {campaigns.length > 0 &&
+        campaigns
+          .slice()
+          .reverse()
+          .map((campaign) => (
+            <Link to={`/campaign/${campaign.id}`} key={campaign.id}>
+              <div className={load ? 'tab-img-container' : 'tab-img-container skeleton'}>
+                <img style={{ visibility: load ? 'visible' : 'hidden' }} src={campaign.imageURL} alt="" />
+                {load && <p className="tab-img-desc text-overflow-hide">{campaign.title}</p>}
+              </div>
+            </Link>
+          ))}
+    </div>
+  );
+};
 
-export default TabBody
+export default TabBody;
---
+++ src/components/MyCampaigns/redux/userSlice.js
@@ -0,0 +1,23 @@
+// userSlice.js
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  currentUser: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setCurrentUser: (state, action) => {
+      state.currentUser = action.payload;
+    },
+  },
+});
+
+export const { setCurrentUser } = userSlice.actions;
+
+export const selectCurrentUser = state => state.user.currentUser;
+
+export default userSlice.reducer;
